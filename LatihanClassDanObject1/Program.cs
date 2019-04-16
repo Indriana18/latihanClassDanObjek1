8f3c745e6daf3edf0052790d85540fcc7f6bba22
indriana18 <indriana.damayanti@students.amikom.ac.id>
2019-04-16 11:57:32 +0700

    LatihanClassDanObjek1

---
+++ LatihanClassDanObject1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membentuk ojek dari class mahasiswa 
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //mengeset nilai properties objek saya dan kamu
+            saya.Nim = "12345";
+            saya.Nama = "indri";
+            saya.Ipk = "3.92f";
+
+            kamu.Nim = "54321";
+            kamu.Nama = "ana";
+            kamu.Ipk = "3.70f";
+
+            //memanggil method registrasi dan Isi Krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
